18b72138f5d5accbd48f50f07a4adf009ddd40d4
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-06-19 13:46:26 +0200

    correciones consulta 9 y 16

--- mongoDB_queries/consulta.restaurants.js
+++ mongoDB_queries/consulta.restaurants.js
@@ -25,7 +25,7 @@ db.restaurants.find({}, {borough: "Bronx", name: 1}).limit(5).skip(5)
 db.restaurants.find({ "grades.score": { $gt: 90 } },{ name: 1, _id: 0 })
 
 /* 9- Escribe una consulta para encontrar los restaurantes que tienen un *score más grande que 80 pero menos que 100 */
-db.restaurants.find({ $and: [ {"grades.score": { $gt: 80 } } , { "grades.score": { $lt: 100 } } ] },{ name: 1, _id: 0 })
+db.restaurants.find({ grades: { $elemMatch: { score: { $gt: 80, $lt: 100 } } }},{ name: 1, "grades.score": 1, _id: 0 })  
 
 /* 10- Escribe una consulta para encontrar los restaurantes que están situados en una longitud inferior a -95.754168 */ 
 db.restaurants.find({ "address.coord": { $lt: -95.754168  } },  { name: 1, _id: 0 })
@@ -46,7 +46,7 @@ db.restaurants.find({ name: { $regex: /^Wil/ } },{ restaurant_id: 1, name: 1, bo
 db.restaurants.find({ name: { $regex: /ces$/ } },{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 })
 
 /* 16- Escribe una consulta para encontrar el restaurante_*id, *name, *borough y *cuisine para aquellos restaurantes que contienen 'Riego' en cualquier lugar de su nombre */
-db.restaurants.find({ name: { $regex: /Riego/i } },{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 })
+db.restaurants.find({ name: { $regex: /Reg/i } },{ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 })
 
 /* 17- Escribe una consulta para encontrar los restaurantes que pertenecen al *Bronx y preparan platos Americanos o chinos */
 db.restaurants.find({ borough: "Bronx", cuisine: { $in: ["American", "Chinese"] } }, { name: 1, borough: 1, cuisine: 1, _id: 0})
